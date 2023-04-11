e5f427bc5a308f27ca7f83390cb218747250e871
Emerson Nascimento <61097794+Emerson05@users.noreply.github.com>
2023-04-11 15:27:03 -0300

    Add files via upload

---
+++ assets/js/empresa3.js
@@ -0,0 +1,16 @@
+
+function voltar(){
+    const btnvoltar3 = document.getElementById('voltar-empresa3')
+
+
+    btnvoltar3.addEventListener('click', () => {
+    window.location.href = "empresa2.html";
+
+});
+}
+
+voltar()
+
+
+
+
